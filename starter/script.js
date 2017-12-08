0c33af3a94a2f32855d64885bf8ea94cf306ce4c
Ryan OKeefe <ryanokeefe208@gmail.com>
2017-12-08 09:47:48 -0500

    barely started

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,17 @@
 // put code here!
+var answer = prompt("Hello, Jack O'Niell. Would you like to visit a known planet or unknown planet? (k/u/neither)")
+ if (answer === 'k' || answer === 'K') {
+        answer = prompt('would you like to visit an advanced planet, a primitive planet, or a random planet? (a/p/r)')
+    if (answer === 'a' || answer === 'A') {
+        answer = prompt("Cool! You've travelled to an advanced planet. There seems to be an alien species that would like to communicate with you, but they do not speak your language. Would you like to let Daniel translate? (y/n)")
+        if (answer === 'y' || answer === 'Y') {
+            answer = prompt('Daniel attempted to translate ')
+        }
+    }
+ 
+
+
+} else if (answer === 'u' || answer === 'U') {
+
+
+} else if (answer === 'neither' || answer === 'Neither')
